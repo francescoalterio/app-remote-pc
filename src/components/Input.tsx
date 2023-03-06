a1192f05a1817fbc60a930e763eb8585be9ea7d7
francescoalterio <francescoalteriog@gmail.com>
2023-03-06 11:14:51 -0400

    write system

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TextInput, Text, StyleSheet } from "react-native";
 import { COLORS } from "../../config/colors";
+import { SocketEmits } from "../types";
 
-export function Input() {
+interface Props {
+  socketEmit: (emit: SocketEmits, msg: string) => void;
+}
+
+export function Input({ socketEmit }: Props) {
   const [text, setText] = useState<string>("");
+  const [prevText, setPrevText] = useState<string>("");
+
+  useEffect(() => {
+    if (!text) setText(" ");
+    if (text || prevText) {
+      if (text.length > prevText.length) {
+        socketEmit("write", text[text.length - 1]);
+      } else {
+        socketEmit("write", "backspace ");
+      }
+    }
+  }, [text]);
+
+  const handleChangeText = (x: string) => {
+    setPrevText(text);
+    setText(x);
+  };
+
+  const handleBlur = () => {
+    setText("");
+    setPrevText("");
+  };
+
   return (
     <TextInput
       style={styles.input}
       cursorColor="red"
       selectionColor={"red"}
       value={text}
-      onChangeText={(x) => setText(x)}
-      onBlur={() => setText("")}
+      onChangeText={(x) => handleChangeText(x)}
+      onBlur={handleBlur}
     ></TextInput>
   );
 }
